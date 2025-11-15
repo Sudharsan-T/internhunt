ab60dc97aaaaad388e47e25312f39bb1970796fd
Sudharsan <sudharsant44@gmail.com>
2025-11-16 01:39:33 +0530

    feat : Get started Frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md text-center">
+        <h1 className="text-3xl font-bold mb-4 text-gray-800">
+          InternHunt
+          <h1 className="text-3xl font-bold text-blue-600">Tailwind Works!</h1>
+        </h1>
+
+        <p className="text-gray-600 text-lg">
+          Your platform to track internships, applications and progress.
         </p>
+
+        <button className="mt-6 px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
+          Get Started
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
